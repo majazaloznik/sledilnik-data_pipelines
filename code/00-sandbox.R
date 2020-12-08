dd94d5739cc1b2afcf942371b35c91e7ac1a042d
mz <maja.zaloznik@gmail.com>
2020-12-08 03:11:08 +0100

    draft sankey

--- code/00-sandbox.R
+++ code/00-sandbox.R
@@ -3,18 +3,32 @@ library(networkD3)
 library(htmlwidgets)
 
 # test
-test <- jsonlite::fromJSON("data/test.json")
+test <- jsonlite::fromJSON("data/master.json")
+
+links <- test$links
+map = setNames(c(0: (length(test$nodes$name)-1)), c(test$nodes$name))
+links$source <- map[unlist(test$links$source)]
+links$target <- map[unlist(test$links$target)]
 
 
-p <- sankeyNetwork(Links = test$links, Nodes = test$nodes, Source = "source",
+my_color <- 'd3.scaleOrdinal() .domain(["1", "2"]) .range(["#69b3a2", "steelblue"])'
+
+
+p <- sankeyNetwork(Links = links, Nodes = test$nodes, Source = "source",
                    Target = "target", Value = "value", NodeID = "name",
+                   sinksRight	= FALSE,
+                  
+                   #LinkGroup = "group", colourScale=my_color,
                    fontSize = 12, nodeWidth = 30, NodeGroup = "group", nodePadding = 20, iterations = 100)
 p
 
 
+
+
+
 # save the widget
 # some weird bug, won't take relative file paths
-f<-"outputs/index.html"
+f<-"docs/index.html"
 saveWidget(p,file.path(normalizePath(dirname(f)),
                                      basename(f)))
 
