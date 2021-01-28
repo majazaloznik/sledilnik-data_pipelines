17e30da9ab12e8fd4d8c740d64c7bf119347a152
mz <maja.zaloznik@gmail.com>
2021-01-28 16:25:30 +0100

    Link colours still not working

--- code/00-sandbox.R
+++ code/00-sandbox.R
@@ -9,25 +9,23 @@ links <- test$links
 map = setNames(c(0: (length(test$nodes$name)-1)), c(test$nodes$name))
 links$source <- map[unlist(test$links$source)]
 links$target <- map[unlist(test$links$target)]
+links$group <- as.factor(map[unlist(test$links$group)])
 
-
-my_color <- 'd3.scaleOrdinal() .domain(["1", "2"]) .range(["#69b3a2", "steelblue"])'
-
+my_color <- 'd3.scaleOrdinal() .domain(["1", "2"]) .range(["red", "purple"])'
 
 p <- sankeyNetwork(Links = links, Nodes = test$nodes, Source = "source",
                    Target = "target", Value = "value", NodeID = "name",
                    sinksRight	= FALSE,
-                   #LinkGroup = "group", colourScale=my_color,
-                   fontSize = 18, nodeWidth = 45, NodeGroup = "group", nodePadding = 13, iterations = 100)
+                   # LinkGroup = "group",
+                   # colourScale=my_color,
+                   fontSize = 18, nodeWidth = 45, 
+                   NodeGroup = "group", 
+                   nodePadding = 13, iterations = 100)
 p
-
-
-
-
 
 # save the widget
 # some weird bug, won't take relative file paths
-f<-"docs/index.html"
+f <- "docs/index.html"
 saveWidget(p,file.path(normalizePath(dirname(f)),
                                      basename(f)))
 
