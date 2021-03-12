fc98d08b8b8c2e15cc8ae0ba214614350461109d
mz <maja.zaloznik@gmail.com>
2021-03-12 02:36:12 +0100

    Update pipelines

--- code/00-sandbox.R
+++ code/00-sandbox.R
@@ -11,17 +11,19 @@ links$source <- map[unlist(test$links$source)]
 links$target <- map[unlist(test$links$target)]
 links$group <- as.character(test$links$group)
 
-my_color <- 'd3.scaleOrdinal() .domain(["1", "2", "hos", "icu", "lab", "inst", "report"]) .range(["#85992C", "#E7FF7D", "#CCE65A", "#6C1D99", "#B25AE6", "navy", "orange", "yellow"])'
+my_color <- 'd3.scaleOrdinal() .domain(["1", "2", "3", "4", "5", "6", "10", "11", "13", "hos", "icu", "lab", "inst", "report", "script", "data", "repo", "sole", "cn", "15"]) .
+range(["#de9a5a", "#587634", "#9c8eb7", "#64bcc4", "#449c84","#d1867b",  "gray", "#ffd922", "de2d26", "#CCE65A", "#6C1D99", "#B25AE6", "b11c84", "yellow", "ffd922", "d559b1", "73ccd5", "74a474", "8d5c45", "a07968"])'
 
 p <- sankeyNetwork(Links = links, Nodes = test$nodes, Source = "source",
                    Target = "target", Value = "value", NodeID = "name",
-                   sinksRight	= FALSE,
+                   sinksRight	= TRUE,
                    LinkGroup = "group",
                    colourScale=my_color,
                    fontSize = 18, nodeWidth = 45, 
                    NodeGroup = "group", 
                    nodePadding = 13, iterations = 100)
 p
+
 
 # save the widget
 # some weird bug, won't take relative file paths
